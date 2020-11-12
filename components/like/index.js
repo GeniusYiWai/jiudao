509107db5fc43416c57de3ffb1278a0aec599809
意外 <lpf1152248628@126.com>
2020-11-12 16:22:41 +0800

    封装like组件

--- components/like/index.js
+++ components/like/index.js
@@ -5,19 +5,44 @@ Component({
    */
   properties: {
 
+    //点赞状态
+    like: {
+      type: Boolean,
+      // value: false,
+      // observer:()=>{
+
+      // }
+    },
+    //点赞数量
+    count: {
+      type: Number
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    //已点赞图片路径
+    yesSrc: 'images/like.png',
+    //未点赞图片路径
+    noSrc: 'images/like@dis.png'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    //点赞事件处理
+    onLike(event) {
+      let like = this.properties.like;
+      let count = this.properties.count;
+      count = like ? count - 1 : count + 1;
+      this.setData({
+        count,
+        like: !like
+      })
 
+    }
   }
 })
